ae93acc9e161903804e6e6f5a88682b1844357ef
artemgur <artemgur01@gmail.com>
2022-10-05 23:11:12 +0300

    Update extrapolation.py

--- ProofOfConcept/extrapolation.py
+++ ProofOfConcept/extrapolation.py
@@ -13,7 +13,8 @@ def extrapolate(data_history, seconds_between_snapshots, seconds_before_extrapol
         row_history_timestamp_index = n_values_with_step(row_history.shape[0], seconds_between_snapshots)
         columns_index = np.arange(0, row_history.shape[1])
 
-        interp = RegularGridInterpolator([row_history_timestamp_index, columns_index], row_history, method='linear', bounds_error=False, fill_value=None)
+        interp = RegularGridInterpolator([row_history_timestamp_index, columns_index], row_history,
+                                         method='linear', bounds_error=False, fill_value=None)
 
         extrapolation_timestamp = row_history_timestamp_index[-1] + seconds_before_extrapolation
         points_to_calculate = list(map(lambda x: (extrapolation_timestamp, x), columns_index))
