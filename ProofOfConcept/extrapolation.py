f62e5e0bba74530082ac3b37647dc3809295aecb
artemgur <artemgur01@gmail.com>
2022-10-05 23:07:14 +0300

    Update main.py, extrapolation.py, and generate_data.py

--- ProofOfConcept/extrapolation.py
+++ ProofOfConcept/extrapolation.py
@@ -8,11 +8,15 @@ def extrapolate(data, seconds_between_snapshots, seconds_before_extrapolation):
     extrapolated = np.empty_like(data[-1])
     for data_row in range(data.shape[1]):
         row_history = data[:,data_row,:]
-        row_history_time_index = n_values_with_step(row_history.shape[0], seconds_between_snapshots)
+
+        row_history_timestamp_index = n_values_with_step(row_history.shape[0], seconds_between_snapshots)
         columns_index = np.arange(0, row_history.shape[1])
-        interp = RegularGridInterpolator([row_history_time_index, columns_index], row_history, method='linear', bounds_error=False, fill_value=None)
-        extrapolation_time = row_history_time_index[-1] + seconds_before_extrapolation
-        points = list(map(lambda x: (extrapolation_time, x), columns_index))
-        result = interp(points)
+
+        interp = RegularGridInterpolator([row_history_timestamp_index, columns_index], row_history, method='linear', bounds_error=False, fill_value=None)
+
+        extrapolation_timestamp = row_history_timestamp_index[-1] + seconds_before_extrapolation
+        points_to_calculate = list(map(lambda x: (extrapolation_timestamp, x), columns_index))
+
+        result = interp(points_to_calculate)
         extrapolated[data_row] = result
     return extrapolated
--- ProofOfConcept/generate_data.py
+++ ProofOfConcept/generate_data.py
@@ -9,12 +9,11 @@ def generate_random_matrix(rows_count, columns_count, multiplier=1):
     return np.random.randn(rows_count, columns_count) * multiplier
 
 
-# noinspection PyShadowingBuiltins
 def generate_next(previous_data, description, row_change_function):
-    next = np.empty_like(previous_data[-1])
-    for row in range(next.shape[0]):
-        next[row] = row_change_function(previous_data[-1][row], description[row])
-    previous_data.append(next)
+    next_ = np.empty_like(previous_data[-1])
+    for row in range(next_.shape[0]):
+        next_[row] = row_change_function(previous_data[-1][row], description[row])
+    previous_data.append(next_)
 
 
 def generate(count, rows_count, columns_count, multiplier=1, row_change_function = row_change_function_linear_simple):
